705dbca4cf23a6cb4716771b81a6a1bd09c6a65f
Ranjan-Sabu <ranjansabu6@gmail.com>
2024-01-06 09:16:48 +0530

    new update

--- organic_products/settings.py
+++ organic_products/settings.py
@@ -80,14 +80,24 @@ AUTH_USER_MODEL = "Accounts.Registration"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "NAME": "Ecommerce_db",
+#         "USER": "postgres",
+#         "PASSWORD": 12345,
+#         "HOST": "localhost",
+#         "PORT": "5432",
+#     }
+# }
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "Ecommerce_db",
-        "USER": "postgres",
-        "PASSWORD": 12345,
-        "HOST": "localhost",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ecommerce',
+        'USER': 'mysuperuser',
+        'PASSWORD': 'mysuperuser',
+        'HOST': 'backend.cfk22qwq6bbd.ap-south-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
