5cc9ede38b3bada114dbd670440d515a7aea4134
Ranjan-Sabu <ranjansabu6@gmail.com>
2024-01-11 09:11:02 +0530

    third

--- organic_products/settings.py
+++ organic_products/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY =config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['52.66.196.233']
 
 
 # Application definition
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     "carts",
     "order",
     "admin_panel",
-    'storages'
 ]
 
 MIDDLEWARE = [
@@ -98,10 +97,11 @@ DATABASES = {
         'NAME': 'ecommerce',
         'USER': config('USER'),
         'PASSWORD': config('PASSWORD'),
-        'HOST': 'backend.cfk22qwq6bbd.ap-south-1.rds.amazonaws.com',
+        'HOST': config('HOST'),
         'PORT': '5432',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -164,12 +164,15 @@ EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 
 
-AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY')
-AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS')
+
+AWS_ACCESS_KEY=config('AWS_ACCESS_KEY')
+AWS_SECRET_ACCESS=config('AWS_SECRET_ACCESS')
 AWS_STORAGE_BUCKET_NAME = 'organicfood'
-AWS_S3_SIGNATURE_NAME = 's3v4',
+AWS_S3_SIGNATURE_NAME = ('s3v4',)
 AWS_S3_REGION_NAME = 'ap-south-1'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL =  None
 AWS_S3_VERITY = True
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+
